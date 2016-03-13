67177a289f47ef676bade69c7b38e63e8a03d635
tylere5 <ewanchuk.tyler@gmail.com>
2016-03-12 16:38:17 -0800

    Added fonts, about and contact pages styled

--- wp-content/themes/redstarter-master/about.php
+++ wp-content/themes/redstarter-master/about.php
@@ -15,15 +15,17 @@ get_header(); ?>
         </header>
 
         <div class="about-content-container">
-          <div class="about-content-left">
+          <div class="about-left">
             <img src="<?php echo get_template_directory_uri() ?>/images/team.jpg" alt="Le Red Bread team">
             <h3>Le Red Bread Team</h3>
-            <?php echo CFS()->get( 'team_copy' ); ?>
+            <p class="team-memo"><?php echo CFS()->get( 'team_memo' ); ?></p>
+            <p class="about-text-left"><?php echo CFS()->get( 'team_copy' ); ?></p>
           </div>
 
-          <div class="about-content-right">
+          <div class="about-right">
             <img src="<?php echo get_template_directory_uri() ?>/images/bakery.jpg" alt="Le Red Bread Bakery">
             <h3>Le Red Bread Bakery</h3>
+            <p class="baker-memo"><?php echo CFS()->get( 'baker_memo' ); ?></p>
             <p class="about-text-right"><?php echo CFS()->get( 'bakery_copy' ); ?></p>
           </div>
         </div>
@@ -32,14 +34,13 @@ get_header(); ?>
           <h2>Our Story</h2>
           <?php echo CFS()->get ( 'our_story' ); ?>
         </div>
+        <section class="redirect-section">
+          <p>
+            <span>Feel free to contact us with any questions comments or suggestions. We even take custom orders!</span>
+            <a href="/contact/" class="front-button">Contact Us</a>
+          </p>
+        </section>
       </div>
-
-      <section class="redirect-section">
-        <p>
-          <span>Feel free to contact us with any questions comments or suggestions. We even take custom orders!</span>
-          <a href="/contact/" class="front-button">Contact Us</a>
-        </p>
-      </section>
 
     </main>
   </div>
--- wp-content/themes/redstarter-master/contact.php
+++ wp-content/themes/redstarter-master/contact.php
@@ -5,17 +5,23 @@
 
 get_header(); ?>
 
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+  <div class="page-container">
+    <div id="primary" class="content-area">
+      <div class="contact-content-area">
+        <main id="main" class="site-main" role="main">
 
-      <?php while ( have_posts() ) : the_post(); ?>
+          <?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+    				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
+    			<?php endwhile; // End of the loop. ?>
 
-    </main>
+        </main>
+      </div>
+    </div>
+
+    <div class="sidebar-area">
+      <?php get_sidebar(); ?>
+    </div>
   </div>
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
