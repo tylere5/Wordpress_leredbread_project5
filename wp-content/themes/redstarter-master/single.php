812377f857d3fc5ce862f38cfa8a6d1ba7c93e10
tylere5 <ewanchuk.tyler@gmail.com>
2016-03-13 19:44:21 -0700

    Finished styling single.php

--- wp-content/themes/redstarter-master/single.php
+++ wp-content/themes/redstarter-master/single.php
@@ -9,26 +9,27 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<div class="blog-single-container">
+				<div class="blog-single-content-area">
+					<?php while ( have_posts() ) : the_post(); ?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+						<?php get_template_part( 'template-parts/content', 'single' ); ?>
 
-			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+						<?php
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+						?>
 
-			<?php the_post_navigation(); ?>
-
-			<?php
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-			?>
-
-			<button type="button" id="close-comments">Close Comments</button>
-
-		<?php endwhile; // End of the loop. ?>
+						<?php endwhile; // End of the loop. ?>
+				</div>
+				<div class="sidebar-area">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
