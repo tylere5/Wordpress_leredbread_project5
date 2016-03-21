75eaaea2306ab01a5a112d909777fc4965471fca
tylere5 <ewanchuk.tyler@gmail.com>
2016-03-20 21:25:23 -0700

    Enqueued font awesome, removed script tag from header

--- wp-content/themes/redstarter-master/functions.php
+++ wp-content/themes/redstarter-master/functions.php
@@ -115,6 +115,13 @@ function red_starter_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'red_starter_scripts' );
 
+
+add_action( 'wp_enqueue_scripts', 'enqueue_font_awesome' );
+function enqueue_font_awesome() {
+
+    wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css' );
+
+}
 /**
  * Custom template tags for this theme.
  */
